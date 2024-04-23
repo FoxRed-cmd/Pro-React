270e7dc6a3c8f372cb0b7d10daa3e0bff38302eb
FoxRed-cmd <blinovk955@gmail.com>
2024-04-23 23:54:59 +0300

    Config for routes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,8 @@
 import "app/styles/main.scss";
-import { Suspense } from "react";
-import { Route, Routes, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useTheme } from "app/providers/ThemeProvider";
-import { MainPage } from "pages/MainPage/index";
 import { classNames } from "shared/lib/classNames/classNames";
-import { AboutPage } from "pages/AboutPage/index";
+import { AppRouter } from "app/providers/router";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
@@ -14,12 +12,7 @@ const App = () => {
       <Link to={"/"}>Главная</Link>
       <Link to={"/about"}>О сайте</Link>
       <button onClick={toggleTheme}>Изменить тему</button>
-      <Suspense>
-        <Routes>
-          <Route path={"/"} element={<MainPage />} />
-          <Route path={"/about"} element={<AboutPage />} />
-        </Routes>
-      </Suspense>
+      <AppRouter />
     </div>
   );
 };
